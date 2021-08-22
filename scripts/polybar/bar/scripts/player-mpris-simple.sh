7a23e165659c9b5375facdb4df456faa8f549308
lckdscl <lckdscl+github@protonmail.com>
2021-08-22 15:20:09 +0100

    Minor edits to some scripts

--- scripts/polybar/bar/scripts/player-mpris-simple.sh
+++ scripts/polybar/bar/scripts/player-mpris-simple.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-player_status=$(playerctl status 2> /dev/null)
+player_status=$(playerctl -p ncspot status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-	if [[ $(playerctl metadata --format "{{artist}}" 2> /dev/null) ]]; then
-		echo "$(playerctl metadata --format "{{artist}} - {{title}}" 2> /dev/null)"
+	if [[ $(playerctl -p ncspot metadata --format "{{artist}}" 2> /dev/null) ]]; then
+		echo "$(playerctl -p ncspot metadata --format "{{artist}} - {{title}}" 2> /dev/null)"
 	else
-		echo "$(playerctl metadata --format "{{title}}" 2> /dev/null)"
+		echo "$(playerctl -p ncspot metadata --format "{{title}}" 2> /dev/null)"
 	fi
 elif [ "$player_status" = "Paused" ]; then
     echo "Paused"
