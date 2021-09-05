d3db443195401edf50651672f32ae5b60350c0cd
lckdscl <lckdscl+github@protonmail.com>
2021-09-05 23:40:19 +0100

    Add option to not git add all and commit right away, make colourful

--- push.sh
+++ push.sh
@@ -5,37 +5,64 @@ USERSTYLEDIR=$HOME/scripts/userstyles
 SCRIPTDIR=$HOME/scripts
 CSS=$HOME/.config/BetterDiscord/themes/pywal-discord-default.theme.css
 SNIPS=$HOME/.config/nvim/ultisnips
-
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;90m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+cat .bpytop-replace.conf > bpytop.conf
 cat .termshark-replace.toml > termshark.toml
 
-echo "==> Copying nvim snippets... "
+echo -e "${DARKGRAY}==>${YELLOW} Copying nvim snippets... "
 cp -R $SNIPS/ $DOTDIR/
-echo "==> Copying Discord userstyles... "
+echo -e "${DARKGRAY}  ->${GREEN} Done."
+echo -e "${DARKGRAY}==>${YELLOW} Copying Discord userstyles... "
 cp $USERSTYLEDIR/template.user.css $USERSTYLEDIR/pywal.user.css
 cat "$CSS" >> "$USERSTYLEDIR/pywal.user.css" && echo "}" >> $USERSTYLEDIR/pywal.user.css
-echo "  -> Done."
-echo "==> Copying wpgtk templates..."
-mv $SCRIPTDIR/templates $HOME/.local/share/Trash/files/
+echo -e "${DARKGRAY}  ->${GREEN} Done."
+echo -e "${DARKGRAY}==>${YELLOW} Copying wpgtk templates..."
 cp -R $HOME/.config/wpg/templates $SCRIPTDIR
-echo "  -> Done."
-echo "==> Copying scripts to repository"
-mv $DOTDIR/scripts $HOME/.local/share/Trash/files/
+echo -e "${DARKGRAY}  ->${GREEN} Done."
+echo -e "${DARKGRAY}==>${YELLOW} Copying scripts to repository"
 cp -R $SCRIPTDIR $DOTDIR
-echo "  -> Done."
-echo "==> Changing working directory and performing git actions..."
-cd $DOTDIR
-echo "==> Printing git status..."
-git status
-message=""
-echo "==> Enter commit message... "
-while [ -z "$message" ]
-do
-    IFS= read -r message
-    wait
-    sleep 1
-done
-git add . && git commit -m "$message"
-git push -u origin main
-git push backup main
-echo "  -> Done, returning to original directory..."
+echo -e "${DARKGRAY}  ->${GREEN} Done."
+echo -e "${DARKGRAY}==>${YELLOW} Would you like to perform git actions now? [y/n]${WHITE}"
+prompt=""
+read -p "Answer: " $prompt
+if [[ "$prompt" == "y" ]]
+then
+    echo -e "${DARKGRAY}==>${YELLOW} Changing working directory and performing git actions..."
+    cd $DOTDIR
+    echo -e "${DARKGRAY}==>${YELLOW} Printing git status..."
+    git status
+    message=""
+    echo -e "${DARKGRAY}==>${YELLOW} Enter commit message...${WHITE}"
+    while [ -z "$message" ]
+    do
+        IFS= read -r message
+        wait
+        sleep 1
+    done
+    git add . && git commit -m "$message"
+    git push -u origin main
+    git push backup main
+else
+    if [[ "$prompt" == "n" ]]
+    then
+        echo -e "${DARKGRAY}  ->${YELLOW} Okay, remember to review the changes then."
+    fi
+fi
+
+echo -e "${DARKGRAY}  ->${YELLOW} Done, returning to original directory..."
 cd $CURRENTDIR
