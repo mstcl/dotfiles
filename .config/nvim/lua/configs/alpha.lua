57052a2107805d4760a767ee6464c9f57ba24a90
lckdscl <lckdscl@protonmail.com>
2021-09-11 01:38:43 +0100

    Continuous vim pimping

--- .config/nvim/lua/configs/alpha.lua
+++ .config/nvim/lua/configs/alpha.lua
@@ -6,26 +6,45 @@ end
 local header = {
     type = "text",
     val = {
-[[               ______                ]],
-[[          _.-""      ""-._           ]],
-[[       .-'                `-.        ]],
-[[     .'      __.----.__      `.      ]],
-[[    /     .-"          "-.     \     ]],
-[[   /    .'                `.    \    ]],
-[[  J    /                    \    L   ]],
-[[  F   J                      L   J   ]],
-[[ J    F     ┌──────────┐     J    L  ]],
-[[ |   J      │  nvLeet  │      L   |  ]],
-[[ |   |      └──────────┘      |   |  ]],
-[[ |   J                        F   |  ]],
-[[ J    L                      J    F  ]],
-[[  L   J   .-""""-.           F   J   ]],
-[[  J    \ /        \   __    /    F   ]],
-[[   \    (|)(|)_   .-'".'  .'    /    ]],
-[[    \    \   /_>-'  .<_.-'     /     ]],
-[[     `.   `-'     .'         .'      ]],
-[[       `--.|___.-'`._    _.-'        ]],
-[[           ^         """"            ]],
+-- [[               ______                ]],
+-- [[          _.-""      ""-._           ]],
+-- [[       .-'                `-.        ]],
+-- [[     .'      __.----.__      `.      ]],
+-- [[    /     .-"          "-.     \     ]],
+-- [[   /    .'                `.    \    ]],
+-- [[  J    /                    \    L   ]],
+-- [[  F   J                      L   J   ]],
+-- [[ J    F     ┌──────────┐     J    L  ]],
+-- [[ |   J      │  nvLeet  │      L   |  ]],
+-- [[ |   |      └──────────┘      |   |  ]],
+-- [[ |   J                        F   |  ]],
+-- [[ J    L                      J    F  ]],
+-- [[  L   J   .-""""-.           F   J   ]],
+-- [[  J    \ /        \   __    /    F   ]],
+-- [[   \    (|)(|)_   .-'".'  .'    /    ]],
+-- [[    \    \   /_>-'  .<_.-'     /     ]],
+-- [[     `.   `-'     .'         .'      ]],
+-- [[       `--.|___.-'`._    _.-'        ]],
+-- [[           ^         """"            ]],
+
+[[                                                                              ]],
+[[                                    ██████                                    ]],
+[[                                ████▒▒▒▒▒▒████                                ]],
+[[                              ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                              ]],
+[[                            ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                            ]],
+[[                          ██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒                              ]],
+[[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▓▓▓▓                          ]],
+[[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▒▒▓▓                          ]],
+[[                        ██▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ██                        ]],
+[[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+[[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+[[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+[[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+[[                        ██▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒██                        ]],
+[[                        ████  ██▒▒██  ██▒▒▒▒██  ██▒▒██                        ]],
+[[                        ██      ██      ████      ████                        ]],
+[[                                                                              ]],
+[[                                                                              ]],
     },
     opts = {
         position = "center",
@@ -37,25 +56,52 @@ local handle = io.popen('fd -d 2 . $HOME"/.local/share/nvim/site/pack/packer" | 
 local plugins = handle:read("*a")
 handle:close()
 
-local thingy = io.popen('echo "$(date +%_a%_e) $(date +%b) $(date +%Y)" | tr -d "\n"')
+local thingy = io.popen('echo "$(date +%a) $(date +%d) $(date +%b)" | tr -d "\n"')
 local date = thingy:read("*a")
 thingy:close()
 
-local footer = {
+local plugin_count = {
     type = "text",
-    val = "  Detected " .. plugins .. " plugins",
+    val = "└─   " .. plugins .. " plugins in total ─┘",
     opts = {
         position = "center",
-        hl = "SpecialKey",
+        hl = "Folded",
     }
 }
 
 local heading = {
     type = "text",
-    val = "  Today is " .. date .. "",
+    val = "┌─   Today is " .. date .. " ─┐",
     opts = {
         position = "center",
-        hl = "SpecialKey",
+        hl = "Folded",
+    }
+}
+
+local top_bar = {
+    type = "text",
+    val = "┌──══───══───══───══───══──┐",
+    opts = {
+        position = "center",
+        hl = "Folded",
+    }
+}
+
+local bot_bar = {
+    type = "text",
+    val = "└──══───══───══───══───══──┘",
+    opts = {
+        position = "center",
+        hl = "Folded",
+    }
+}
+
+local footer = {
+    type = "text",
+    val = "-nvLeet-",
+    opts = {
+        position = "center",
+        hl = "AlphaFooter",
     }
 }
 
@@ -67,10 +113,10 @@ local function button(sc, txt, keybind)
         text = txt,
         shortcut = sc,
         cursor = 5,
-        width = 30,
+        width = 24,
         align_shortcut = "right",
-        hl_shortcut = "Folded",
-        hl_text = "Folded",
+        hl_shortcut = "SpecialKey",
+        hl_text = "SpecialKey",
     }
     if keybind then
         opts.keymap = {"n", sc_, keybind, {noremap = true, silent = true}}
@@ -90,11 +136,23 @@ end
 local buttons = {
     type = "group",
     val = {
-    button( "LDR h", "  Recents" , ":Telescope oldfiles<CR>"),
-    button( "LDR b", "  Buffers" , ":Telescope buffers<CR>"),
-    button( "LDR b", "  Browse" , ":Telescope file_browser<CR>"),
-    button( "MRK V", "  Settings" , ":execute 'normal! `V'<CR>"),
-    button( "MRK P", "  Plugins" , ":execute 'normal! `P'<CR>"),
+    --[[ button( "│ ", " │" , ":echo ''<CR>"),
+    button( "LDR h   ║", "║     Recents" , ":Telescope oldfiles<CR>"),
+    button( "│ ", " │" , ":echo ''<CR>"),
+    button( "LDR b   ║", "║     Buffers" , ":Telescope buffers<CR>"),
+    button( "│ ", " │" , ":echo ''<CR>"),
+    button( "LDR b   ║", "║     Browse" , ":Telescope file_browser<CR>"),
+    button( "│ ", " │" , ":echo ''<CR>"),
+    button( "MRK V   ║", "║     Settings" , ":execute 'normal! `V'<CR>"),
+    button( "│ ", " │" , ":echo ''<CR>"),
+    button( "MRK P   ║", "║     Plugins" , ":execute 'normal! `P'<CR>"),
+    button( "│ ", " │" , ":echo ''<CR>"), ]]
+    button( "LDR h", "   Recents" , ":Telescope oldfiles<CR>"),
+    button( "LDR b", "   Buffers" , ":Telescope buffers<CR>"),
+    button( "LDR y", "   Explore" , ":Telescope file_browser<CR>"),
+    button( "LDR /", "   Ripgrep" , ":Telescope file_browser<CR>"),
+    button( "MRK V", "   Options" , ":execute 'normal! `V'<CR>"),
+    button( "MRK P", "   Plugins" , ":execute 'normal! `P'<CR>"),
     },
     opts = {
         spacing = 1,
@@ -104,8 +162,11 @@ local buttons = {
 local section = {
     header = header,
     buttons = buttons,
-    footer = footer,
+    plugin_count = plugin_count,
     heading = heading,
+    top_bar = top_bar,
+    bot_bar = bot_bar,
+    footer = footer
 }
 
 local opts = {
@@ -114,9 +175,12 @@ local opts = {
         section.header,
         {type = "padding", val = 2},
         section.heading,
-        {type = "padding", val = 1},
+        section.plugin_count,
+        {type = "padding", val = 3},
+        -- section.top_bar,
         section.buttons,
-        {type = "padding", val = 5},
+        -- section.bot_bar,
+        {type = "padding", val = 4},
         section.footer,
     },
     opts = {
