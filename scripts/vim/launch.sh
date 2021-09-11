8c77a2ea67445f4de882b1f4568a255ac9c17d96
lckdscl <lckdscl@protonmail.com>
2021-09-11 01:40:10 +0100

    Modified to allow chdir

--- scripts/vim/launch.sh
+++ scripts/vim/launch.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 0 ]; then
     kitty @ --to unix:/tmp/mykitty set-spacing --match title:nvim padding=0
 else
     if [ "$#" -eq 0 ]; then
-        kitty @ --to unix:/tmp/mykitty launch --type=tab --tab-title "nvim" nvim
+        kitty @ --to unix:/tmp/mykitty launch --type=tab --tab-title "nvim" --cwd "$PWD" nvim
         sleep 0.1
         kitty @ --to unix:/tmp/mykitty set-spacing --match title:nvim padding=0
     fi
