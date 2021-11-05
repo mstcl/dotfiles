512724aa7e770fd9867889823ed4c8950180edf3
lckdscl <lckdscl+github@protonmail.com>
2021-11-05 15:58:53 +0000

    Fix: showing correct active tasks

--- .config/polybar/bar/scripts/get-tasks.sh
+++ .config/polybar/bar/scripts/get-tasks.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-num=$(task rc.verbose=nothing active | wc -l)
+num=$(task rc.verbose=nothing rc.report.next.columns=description.desc rc.report.next.labels= rc.defaultwidth=1000 next +ACTIVE | wc -l)
 if [ "$num" -gt "0" ]
 then
     echo " " 2>/dev/null
