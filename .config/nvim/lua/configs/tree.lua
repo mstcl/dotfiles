c648d8367f80d26bea2ecdd46a38dfa519445915
lckdscl <lckdscl+github@protonmail.com>
2021-11-30 20:09:26 +0000

    Change: Use indent markers instead

--- .config/nvim/lua/configs/tree.lua
+++ .config/nvim/lua/configs/tree.lua
@@ -1,3 +1,13 @@
+local g = vim.g
+
+g.nvim_tree_highlight_opened_files = 1
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
+g.nvim_tree_window_picker_exclude = {
+   filetype = { 'notify', 'packer', 'qf' },
+   buftype = {'terminal' },
+}
+
 require'nvim-tree'.setup {
     disable_netrw       = true,
     hijack_netrw        = true,
@@ -7,6 +17,10 @@ require'nvim-tree'.setup {
     open_on_tab         = true,
     hijack_cursor       = true,
     update_cwd          = false,
+    update_to_buf_dir   = {
+        enable = true,
+        auto_open = true,
+    },
     update_focused_file = {
         enable      = true,
         update_cwd  = true,
