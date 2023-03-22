18fcb95f60ae49b355c1b1427a6f00e14b7e13c6
lckdscl <lckdscl+github@protonmail.com>
2023-03-22 23:38:33 +0000

    Update

--- scripts/rofi/rlaunch-config.sh
+++ scripts/rofi/rlaunch-config.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rlaunch --color0 "#811f34" --color1 "#a8334c" --color2 "#f5f5f5" --color3 "#f5f5f5" --color4 "#a8334c" --font "Liga SFMono Nerd Font" --height 30
+rlaunch --color0 "#811f34" --color1 "#a8334c" --color2 "#f5f5f5" --color3 "#f5f5f5" --color4 "#a8334c" --font "SF Mono" --height 30
