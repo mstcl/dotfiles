0a82b3f0305c03557849af39f2fa156534a2f4d8
code <code@beee.ps>
2025-05-03 00:26:02 +0100

    feat: add niri bindings

--- scripts/src/other/dmenu/dmenu_default.sh
+++ scripts/src/other/dmenu/dmenu_default.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dmenu -i -f --nb "#e9e5e2" --nf "#140014" --sb "#cec6bf" --sf "#140014" --font "SF Pro Text:pixelsize=16" --render_minheight 22 "$@"
+dmenu -i -f --nb "#f1efeb" --nf "#140014" --sb "#cec6bf" --sf "#140014" --font "SF Pro Text:pixelsize=16" --render_minheight 22 "$@"
