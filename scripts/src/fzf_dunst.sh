0c8356e3f1fc58f5e8e47c8427fe76ceee1f9d18
code <code@beee.ps>
2025-05-11 18:39:10 +0100

    chore: add dunst

--- scripts/src/fzf_dunst.sh
+++ scripts/src/fzf_dunst.sh
@@ -2,8 +2,8 @@
 
 source ~/.local/bin/fzf_defaults
 
-makoctl history | jq -r \
+dunstctl history | jq -r \
 	'.data[0][] | "\(.id.data)\t\u001b[1;33m\(.summary.data)\u001b[0m\t\(.appname.data)"' |
 	fzf --ansi -d '\t' --with-nth '2,3' \
-		--preview "makoctl history | jq -c '.data[0][] | select(.id.data == {1})' | jq -r '.body.data'" \
+		--preview "dunstctl history | jq -c '.data[0][] | select(.id.data == {1})' | jq -r '.body.data'" \
 		--border-label='Saved notifications'
