532fe7f661f5c19275f135405381d62466171638
code <code@beee.ps>
2025-09-07 15:04:01 +0100

    fix: maple tweaks

--- scripts/src/focus.sh
+++ scripts/src/focus.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
 wmfocus -f "Maple Mono":172 --bgcolor "#cec6bf" --bgcolorcurrent "#cec6bf" --textcolorcurrent "#a89a91" --textcolor "#493f37" --margin "10" --halign "center" --valign "center"
-
