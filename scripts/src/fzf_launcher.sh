c3efdc57bc41d22aa400c17a9cb52fd4e5f7c647
code <code@beee.ps>
2025-10-11 19:14:33 +0100

    squash! feat: freshen up niri and fzf scripts

--- scripts/src/fzf_launcher.sh
+++ scripts/src/fzf_launcher.sh
@@ -3,8 +3,7 @@
 source ~/.local/bin/fzf_defaults
 
 cmd=$(j4-dmenu-desktop --log-level="ERROR" --skip-i3-exec-check --no-exec \
-	--dmenu="fzf --bind 'enter:accept' \
-	--bind '?:unbind(?)'" |
+	--dmenu="fzf --bind 'enter:accept' --bind '?:unbind(?)'" |
 	tr -d "'")
 
 [[ -n "$cmd" ]] && niri msg action spawn -- "$cmd"
