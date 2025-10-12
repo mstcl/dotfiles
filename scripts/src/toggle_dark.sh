8a3e5078a0404b965ce2b1adc06bccce7409f0ac
code <code@beee.ps>
2025-10-13 00:09:52 +0100

    feat: dark theme toggle

---
+++ scripts/src/toggle_dark.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+current=$(gsettings get org.gnome.desktop.interface color-scheme)
+
+ALACRITTY_CONFIG_FILE="$HOME/.config/alacritty/alacritty.toml"
+GIT_CONFIG_FILE="$HOME/.config/git/config"
+
+if [ "$current" = "'prefer-dark'" ]; then
+	# gsettings
+	gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+
+	# alacritty
+	sed -i "s/tavern/ivory/" "${ALACRITTY_CONFIG_FILE}"
+	sed -i "s/features = tavern/features = ivory/" "${GIT_CONFIG_FILE}"
+else
+	# gsettings
+	gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+	# alacritty
+	sed -i "s/ivory/tavern/" "${ALACRITTY_CONFIG_FILE}"
+	sed -i "s/features = ivory/features = tavern/" "${GIT_CONFIG_FILE}"
+fi
