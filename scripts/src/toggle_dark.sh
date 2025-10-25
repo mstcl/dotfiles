55a9e065919c9c3b13e7b86d3856210d39389814
code <code@beee.ps>
2025-10-25 12:14:38 +0100

    feat: dark theme toggle for nvim and jujutsu

--- scripts/src/toggle_dark.sh
+++ scripts/src/toggle_dark.sh
@@ -4,6 +4,7 @@ current=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 ALACRITTY_CONFIG_FILE="$HOME/.config/alacritty/alacritty.toml"
 GIT_CONFIG_FILE="$HOME/.config/git/config"
+NVIM_CONFIG_FILE="$HOME/.config/nvim/lua/plugin/99_override.lua"
 
 if [ "$current" = "'prefer-dark'" ]; then
 	# gsettings
@@ -12,6 +13,7 @@ if [ "$current" = "'prefer-dark'" ]; then
 	# alacritty
 	sed -i "s/tavern/ivory/" "${ALACRITTY_CONFIG_FILE}"
 	sed -i "s/features = tavern/features = ivory/" "${GIT_CONFIG_FILE}"
+	sed -i "s/tavern/ivory/" "${NVIM_CONFIG_FILE}"
 else
 	# gsettings
 	gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
@@ -19,4 +21,5 @@ else
 	# alacritty
 	sed -i "s/ivory/tavern/" "${ALACRITTY_CONFIG_FILE}"
 	sed -i "s/features = ivory/features = tavern/" "${GIT_CONFIG_FILE}"
+	sed -i "s/ivory/tavern/" "${NVIM_CONFIG_FILE}"
 fi
