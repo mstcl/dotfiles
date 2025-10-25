01b497cc281ed7e6ebecf4b1a788aacf06cabf1d
code <code@beee.ps>
2025-10-25 13:14:51 +0100

    fix: use default venv

--- scripts/src/setup_venv.sh
+++ scripts/src/setup_venv.sh
@@ -9,8 +9,8 @@ uv python install "$PYTHON_VERSION"
 
 mkdir -p "$VENV_DIR"
 
-# --- ansible venv ---
-ANSIBLE_VENV=ansible_venv
-[ -d "$VENV_DIR/$ANSIBLE_VENV" ] || uv venv --python "$PYTHON_VERSION" "$VENV_DIR/$ANSIBLE_VENV"
-source "$VENV_DIR/$ANSIBLE_VENV/bin/activate"
+# --- default venv ---
+DEFAULT_VENV=default
+[ -d "$VENV_DIR/$DEFAULT_VENV" ] || uv venv --python "$PYTHON_VERSION" "$VENV_DIR/$DEFAULT_VENV"
+source "$VENV_DIR/$DEFAULT_VENV/bin/activate"
 uv pip install -r ${HOME}/dotfiles/requirements.txt
