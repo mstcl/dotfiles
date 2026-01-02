c7dd9d8e511e8c635f3629187ba8bca845aae33b
code <code@beee.ps>
2026-01-02 18:07:31 +0000

    fix: gui theme toggle error handling

--- scripts/src/gui_dark_theme.sh
+++ scripts/src/gui_dark_theme.sh
@@ -8,5 +8,5 @@ sed -i "s/features = ivory/features = tavern/" "${GIT_CONFIG_FILE}"
 sed -i "s/ivory/tavern/" "${NVIM_CONFIG_FILE}"
 sed -i "s/initial-color-theme=2/initial-color-theme=1/" "${FOOT_CONFIG_FILE}"
 
-pkill -SIGUSR1 foot
-pkill -SIGUSR1 nvim
+pkill -SIGUSR1 foot || true
+pkill -SIGUSR1 nvim || true
--- scripts/src/gui_light_theme.sh
+++ scripts/src/gui_light_theme.sh
@@ -8,5 +8,5 @@ sed -i "s/features = tavern/features = ivory/" "${GIT_CONFIG_FILE}"
 sed -i "s/tavern/ivory/" "${NVIM_CONFIG_FILE}"
 sed -i "s/initial-color-theme=1/initial-color-theme=2/" "${FOOT_CONFIG_FILE}"
 
-pkill -SIGUSR2 foot
-pkill -SIGUSR1 nvim
+pkill -SIGUSR2 foot || true
+pkill -SIGUSR1 nvim || true
