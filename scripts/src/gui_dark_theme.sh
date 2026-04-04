511909617cc176e77570d6c7f861ae440d9549df
code <code@beee.ps>
2026-04-04 17:12:20 +0100

    feat: update dms

--- scripts/src/gui_dark_theme.sh
+++ scripts/src/gui_dark_theme.sh
@@ -6,7 +6,7 @@ gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 # sed -i "s/ivory/tavern/" "${ALACRITTY_CONFIG_FILE}"
 sed -i "s/features = ivory/features = tavern/" "${GIT_CONFIG_FILE}"
 sed -i "s/ivory/tavern/" "${NVIM_CONFIG_FILE}"
-sed -i "s/initial-color-theme=2/initial-color-theme=1/" "${FOOT_CONFIG_FILE}"
+sed -i "s/initial-color-theme=light/initial-color-theme=dark/" "${FOOT_CONFIG_FILE}"
 
 pkill -SIGUSR1 foot || true
 pkill -SIGUSR1 nvim || true
